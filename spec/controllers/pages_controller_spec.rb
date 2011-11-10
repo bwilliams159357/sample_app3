e821d3158ee50d58ee58bd9af294c18c4a83e8d7
Brian Williams <bwilliam@cs.utsa.edu>
2011-11-10 09:51:44 -0600

    Added titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -22,7 +22,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "KContact")
+      response.should have_selector("title", :content => "Contact")
     end
   end
   
